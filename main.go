59339ba466357f0c333a89cc7a3a78158a03e9c1
Daniel Cooper <cooper.daniel45@gmail.com>
2017-04-30 19:32:58 +0100

    Add http server and action for update

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
+
+	http.HandleFunc("/update", updateQueues)
+
+	log.Fatal(http.ListenAndServe(":9191", nil))
+}
+
+func updateQueues(w http.ResponseWriter, r *http.Request) {
 
 	qs := NewQueueService(&HttpClient{})
 
 	qs.GetQueues()
+
+	log.Printf("Retrieved queues.")
 }
